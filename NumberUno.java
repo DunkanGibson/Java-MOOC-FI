28e9bf5249cc4b336693c435fa0edf553498268e
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-08-20 13:46:05 -0400

    Solved using method to return value

---
+++ NumberUno.java
@@ -0,0 +1,14 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        int number = numberUno();
+        System.out.println(number);
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+
+}
