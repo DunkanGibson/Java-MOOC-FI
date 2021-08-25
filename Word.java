265e84d39330dc8be55af63e267f7a8322e46924
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-08-24 20:26:40 -0400

    Create Word.java

---
+++ Word.java
@@ -0,0 +1,14 @@
+/**
+ * Word
+ */
+public class Word {
+
+    public static void main(String[] args) {
+        System.out.println(word());
+    }
+
+    public static String word() {
+        return "Hello World";
+
+    }
+}
